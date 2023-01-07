76244f1d80c558484477df0a33d725a4a1dbed54
User <User@DESKTOP-1GHJJPL>
2023-01-07 22:45:42 +0530

    added a method to convert JSON to POJO class using ObjectMapper class

--- src/main/java/com/team/ConvertJSONToPOJOClass.java
+++ src/main/java/com/team/ConvertJSONToPOJOClass.java
@@ -1,5 +1,20 @@
 package com.team;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ConvertJSONToPOJOClass {
 
+	public RCBTeam convertJSONFileIntoPOJO() throws StreamReadException, DatabindException, IOException {
+		//Login to convert JSON file into POJO class
+		ObjectMapper objectMapper = new ObjectMapper();
+		RCBTeam rcbTeamDetails = objectMapper.readValue(
+				new File(System.getProperty("user.dir") + "/jsonfile/TeamRCB.json"), RCBTeam.class);
+		return rcbTeamDetails;
+		
+	}
 }
